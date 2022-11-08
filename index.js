e484f4fdcf187d80d27cb9d7fd5f330776e164bd
qiuqfang <2312180277@qq.com>
2022-11-08 15:03:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       You
+// @description  try to take over the world!
+// @homepage, @homepageURL, @website and @source
+// @icon, @iconURL and @defaulticon
+// @icon64 and @icon64URL
+// @updateURL
+// @downloadURL
+// @supportURL
+// @include
+// @match
+// @exclude
+// @require
+// @resource
+// @connect
+// @run-at
+// @grant
+// @antifeature
+// @noframes
+// @unwrap
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
